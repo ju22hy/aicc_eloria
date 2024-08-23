a59a83d5a6771666c0c7e5230503839ac13f6bc6
sora <ju22hy@gmail.com>
2024-08-23 11:06:48 +0900

    test initial

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      ㅁㅁㅁ
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/Main/WeeklySection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WeeklySection = () => {
+  return <div>Weekly</div>;
+};
+
+export default WeeklySection;
