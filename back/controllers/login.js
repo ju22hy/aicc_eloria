9d5ba25fdd9e113f1bc9a0aa6c6fb0be7c04b828
BY1502 <sks14502@gmail.com>
2024-08-26 16:34:36 +0900

    커밋

---
+++ back/controllers/login.js
@@ -0,0 +1,31 @@
+async function login(req, res) {
+  try {
+    // 데이터베이스에서 사용자 조회
+    const result = await pool.query(
+      'SELECT * FROM aicc_5team WHERE email = $email',
+      [email]
+    );
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(401).json({ error: '가입되지 않은 회원입니다.' });
+    }
+
+    // 비밀번호 비교
+    const match = await bcrypt.compare(password, user.password);
+
+    if (match) {
+      res.status(200).json({
+        email: user.email,
+        password: user.password,
+      });
+    } else {
+      res.status(401).json({ error: '이메일 또는 비밀번호가 잘못되었습니다.' });
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ error: '서버 오류' });
+  }
+}
+
+module.exports = { login };
