7c873a10917c7e2f65ce9695a7460f628ab1026f
“dongkyu” <kjuvcz95@gamil.com>
2024-08-27 10:52:21 +0900

    weekly save

--- front/src/components/Main/CelebSection.js
+++ front/src/components/Main/CelebSection.js
@@ -1,7 +1,31 @@
 import React from "react";
+import './Celeb.css';
 
 const CelebSection = () => {
-  return <div>Celeb</div>;
+  return (
+    <body>
+    <div class='section celeb-section'>
+      <div class='title'>
+        <h1>CELEB'S PICK</h1>
+      </div>
+    </div>
+
+    <div class='section image'>
+      <div class='celeb-img'>
+        <img src='' alt='사진1'></img>
+      </div>
+      <div class='celeb-img'>
+        <img src='' alt='사진2'></img>
+      </div>
+      <div class='celeb-img'>
+        <img src='' alt='사진3'></img>
+      </div>
+      <div class='celeb-img'>
+        <img src='' alt='사진4'></img>
+      </div>
+    </div>
+    </body>
+  );
 };
 
 export default CelebSection;
