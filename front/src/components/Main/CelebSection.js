83ac699908c9e4aaec567ed04ed7cea68448dddf
“dongkyu” <kjuvcz95@gamil.com>
2024-08-27 15:35:02 +0900

    celeb css 기능 추가

--- front/src/components/Main/CelebSection.js
+++ front/src/components/Main/CelebSection.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import $ from "jquery"; // jQuery 임포트
 import "./Celeb.css";
 import Weekly1 from "../Weekly_image_sample/weekly-image.jpg";
 import Weekly2 from "../Weekly_image_sample/weeklyimg_ring1.jpg";
 
 const CelebSection = () => {
+  useEffect(() => {
+    // jQuery를 사용하여 .celeb-img에 마우스 효과 추가
+    $(".celeb-img").hover(
+      function () {
+        $(this).addClass("hover");
+      },
+      function () {
+        $(this).removeClass("hover");
+      }
+    );
+  }, []);
+
   return (
     <section>
       <div className="celeb-section">
@@ -16,22 +29,26 @@ const CelebSection = () => {
         <div className="celeb-img 1">
           <img src={Weekly2} alt="사진1"></img>
           <h2>상품명</h2>
-          <p>상품 설명 상품 설명</p>
+          <p className="text">상품 설명 상품 설명</p>
+          <p className="price">KRW 00,000</p>
         </div>
         <div className="celeb-img 2">
           <img src={Weekly1} alt="사진2"></img>
           <h2>상품명</h2>
-          <p>상품 설명 상품 설명</p>
+          <p className="text">상품 설명 상품 설명</p>
+          <p className="price">KRW 00,000</p>
         </div>
         <div className="celeb-img 3">
           <img src={Weekly2} alt="사진3"></img>
           <h2>상품명</h2>
-          <p>상품 설명 상품 설명</p>
+          <p className="text">상품 설명 상품 설명</p>
+          <p className="price">KRW 00,000</p>
         </div>
         <div className="celeb-img 4">
           <img src={Weekly1} alt="사진4"></img>
           <h2>상품명</h2>
-          <p>상품 설명 상품 설명</p>
+          <p className="text">상품 설명 상품 설명</p>
+          <p className="price">KRW 00,000</p>
         </div>
       </div>
     </section>
