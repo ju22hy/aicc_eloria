c5b433df330f567f9dd7d9cfe5bbf3c8743bc102
“dongkyu” <kjuvcz95@gamil.com>
2024-08-27 16:34:05 +0900

    celeb css 수정

--- front/src/components/Product/Product.jsx
+++ front/src/components/Product/Product.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "./Product.css";
 
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <div className="Product-Page">
+      <div className="product-section">
+        <div className="gender">
+          <h1>WOMANS</h1>
+        </div>
+        <div className="item-menu"></div>
+      </div>
+
+      <div className="product-images">
+        <div className="product-img 1">
+          <img src="" alt="사진1"></img>
+          <h2>상품명</h2>
+          <p className="text">상품 설명 상품 설명</p>
+          <p className="price">KRW 00,000</p>
+        </div>
+        <div className="product-img 2">
+          <img src="" alt="사진2"></img>
+          <h2>상품명</h2>
+          <p className="text">상품 설명 상품 설명</p>
+          <p className="price">KRW 00,000</p>
+        </div>
+        <div className="product-img 3">
+          <img src="" alt="사진3"></img>
+          <h2>상품명</h2>
+          <p className="text">상품 설명 상품 설명</p>
+          <p className="price">KRW 00,000</p>
+        </div>
+        <div className="product-img 4">
+          <img src="" alt="사진4"></img>
+          <h2>상품명</h2>
+          <p className="text">상품 설명 상품 설명</p>
+          <p className="price">KRW 00,000</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
