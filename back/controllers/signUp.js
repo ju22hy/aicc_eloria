5af80b4bcee6dfd66d4e6b2a9747f13e584462eb
BY1502 <sks14502@gmail.com>
2024-08-27 13:02:31 +0900

    acc

--- back/controllers/signUp.js
+++ back/controllers/signUp.js
@@ -5,6 +5,17 @@ async function signUp(req, res) {
   const { nickname, email, password, phone_number } = req.body;
   const user_key = 'basket_key';
   try {
+    const existingUser = await database.query(
+      'SELECT * FROM aicc_5team WHERE email = $1 OR nickname = $2 OR phone_number = $3',
+      [email, nickname, phone_number]
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res
+        .status(400)
+        .json({ error: '이미 존재하는 이메일, 닉네임 또는 전화번호입니다.' });
+    }
+
     // 비밀번호 암호화
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -25,8 +36,6 @@ async function signUp(req, res) {
   } catch (error) {
     console.error('Error inserting data:', error);
     if (error.code === '23505') {
-      // Unique violation error code
-      res.status(400).json({ error: '이메일이 이미 존재합니다.' });
     } else {
       res.status(500).json({ error: '서버 오류' });
     }
