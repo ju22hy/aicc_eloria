1ab311607ecd925336627481250ed2a49b2e3798
“dongkyu” <kjuvcz95@gamil.com>
2024-08-27 13:20:00 +0900

    celeb css 추가

--- front/src/components/Main/CelebSection.js
+++ front/src/components/Main/CelebSection.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Celeb.css";
+import Weekly1 from "../Weekly_image_sample/weekly-image.jpg";
+import Weekly2 from "../Weekly_image_sample/weeklyimg_ring1.jpg";
 
 const CelebSection = () => {
   return (
@@ -12,19 +14,23 @@ const CelebSection = () => {
 
       <div className="celeb-images">
         <div className="celeb-img 1">
-          <img src="" alt="사진1"></img>
+          <img src={Weekly2} alt="사진1"></img>
+          <h2>상품명</h2>
           <p>상품 설명 상품 설명</p>
         </div>
         <div className="celeb-img 2">
-          <img src="" alt="사진2"></img>
+          <img src={Weekly1} alt="사진2"></img>
+          <h2>상품명</h2>
           <p>상품 설명 상품 설명</p>
         </div>
         <div className="celeb-img 3">
-          <img src="" alt="사진3"></img>
+          <img src={Weekly2} alt="사진3"></img>
+          <h2>상품명</h2>
           <p>상품 설명 상품 설명</p>
         </div>
         <div className="celeb-img 4">
-          <img src="" alt="사진4"></img>
+          <img src={Weekly1} alt="사진4"></img>
+          <h2>상품명</h2>
           <p>상품 설명 상품 설명</p>
         </div>
       </div>
