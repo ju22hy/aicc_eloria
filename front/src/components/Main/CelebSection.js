53f736a3e654feda7845a09904e1669c03c96807
“dongkyu” <kjuvcz95@gamil.com>
2024-08-27 10:52:37 +0900

    celeb save

--- front/src/components/Main/CelebSection.js
+++ front/src/components/Main/CelebSection.js
@@ -3,28 +3,30 @@ import './Celeb.css';
 
 const CelebSection = () => {
   return (
-    <body>
-    <div class='section celeb-section'>
-      <div class='title'>
+    <div className='celeb-section'>
+      <div className='title'>
         <h1>CELEB'S PICK</h1>
       </div>
-    </div>
 
-    <div class='section image'>
-      <div class='celeb-img'>
-        <img src='' alt='사진1'></img>
-      </div>
-      <div class='celeb-img'>
-        <img src='' alt='사진2'></img>
-      </div>
-      <div class='celeb-img'>
-        <img src='' alt='사진3'></img>
-      </div>
-      <div class='celeb-img'>
-        <img src='' alt='사진4'></img>
+      <div className='celeb-images'>
+        <div className='celeb-img 1'>
+          <img src='' alt='사진1'></img>
+          <p>상품 설명 상품 설명</p>
+        </div>
+        <div className='celeb-img 2'>
+          <img src='' alt='사진2'></img>
+          <p>상품 설명 상품 설명</p>
+        </div>
+        <div className='celeb-img 3'>
+          <img src='' alt='사진3'></img>
+          <p>상품 설명 상품 설명</p>
+        </div>
+        <div className='celeb-img 4'>
+          <img src='' alt='사진4'></img>
+          <p>상품 설명 상품 설명</p>
+        </div>
       </div>
     </div>
-    </body>
   );
 };
 
