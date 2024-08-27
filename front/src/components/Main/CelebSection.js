4acf60d8413bdd040f08e4136834e1337e7ba576
“dongkyu” <kjuvcz95@gamil.com>
2024-08-27 12:27:33 +0900

    test

--- front/src/components/Main/CelebSection.js
+++ front/src/components/Main/CelebSection.js
@@ -1,28 +1,29 @@
 import React from "react";
-import './Celeb.css';
+import "./Celeb.css";
 
 const CelebSection = () => {
   return (
-    <div className='celeb-section'>
-      <div className='title'>
+    <div className="celeb-section">
+      <div className="title">
         <h1>CELEB'S PICK</h1>
+        <p>test</p>
       </div>
 
-      <div className='celeb-images'>
-        <div className='celeb-img 1'>
-          <img src='' alt='사진1'></img>
+      <div className="celeb-images">
+        <div className="celeb-img 1">
+          <img src="" alt="사진1"></img>
           <p>상품 설명 상품 설명</p>
         </div>
-        <div className='celeb-img 2'>
-          <img src='' alt='사진2'></img>
+        <div className="celeb-img 2">
+          <img src="" alt="사진2"></img>
           <p>상품 설명 상품 설명</p>
         </div>
-        <div className='celeb-img 3'>
-          <img src='' alt='사진3'></img>
+        <div className="celeb-img 3">
+          <img src="" alt="사진3"></img>
           <p>상품 설명 상품 설명</p>
         </div>
-        <div className='celeb-img 4'>
-          <img src='' alt='사진4'></img>
+        <div className="celeb-img 4">
+          <img src="" alt="사진4"></img>
           <p>상품 설명 상품 설명</p>
         </div>
       </div>
