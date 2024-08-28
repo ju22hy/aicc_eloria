82668dbafc8996e29c2b17f9d2478820406d1195
sora <ju22hy@gmail.com>
2024-08-28 19:40:44 +0900

    login/out nav 상태 변경

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,46 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('../controllers/google'); // Google Passport 설정 불러오기
-const bcrypt = require('bcrypt');
-const pool = require('../database/database');
+const passport = require("../controllers/google"); // Google Passport 설정 불러오기
+const bcrypt = require("bcrypt");
+const pool = require("../database/database");
 
-router.post('/googleinfo', async (req, res) => {
+router.post("/googleinfo", async (req, res) => {
   const { email, password, contact } = req.body;
   try {
-    const user_key = 'basket_key';
+    const user_key = "basket_key";
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // 데이터베이스 업데이트
     await pool.query(
-      'UPDATE aicc_5team SET user_key = $1, password = $2, phone_number = $3 WHERE email = $4',
+      "UPDATE aicc_5team SET user_key = $1, password = $2, phone_number = $3 WHERE email = $4",
       [user_key, hashedPassword, contact, email]
     );
 
     res.status(200).json({
-      message: '구글로 회원가입이 완료되었습니다.',
+      message: "구글로 회원가입이 완료되었습니다.",
     });
   } catch (error) {
-    console.error('Database error:', error.stack);
-    res.status(500).json({ message: '서버 오류', error: error.message });
+    console.error("Database error:", error.stack);
+    res.status(500).json({ message: "서버 오류", error: error.message });
   }
 });
 
 // Google 로그인 경로
 router.get(
-  '/google',
+  "/google",
   (req, res, next) => {
     // console.log('Google login initiated');
     next();
   },
-  passport.authenticate('google', { scope: ['profile', 'email'] })
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 // Google 로그인 콜백 경로
 router.get(
-  '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
-    res.redirect('http://localhost:3000/googleinfo'); // 로그인 성공 후 리디렉션할 경로
+    res.redirect("http://localhost:3000/googleinfo"); // 로그인 성공 후 리디렉션할 경로
   }
 );
 
