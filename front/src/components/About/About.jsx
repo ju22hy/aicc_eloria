959743e48f56d603e762da511cec9d86ea898974
“dongkyu” <kjuvcz95@gamil.com>
2024-08-28 15:43:09 +0900

    save

--- front/src/components/About/About.jsx
+++ front/src/components/About/About.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./About.css";
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div className="about-page">
+      <div className="about-section">
+        <div className="about-text">
+          <p>ABOUT US</p>
+        </div>
+      </div>
+
+      <div className="about-img">
+        <img src=".logo" alt="대표 이미지" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
