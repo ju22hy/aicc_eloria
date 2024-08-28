ad9f286661f1d78b3c54d6b15a687d660faa2ea1
BY1502 <sks14502@gmail.com>
2024-08-28 11:05:27 +0900

    ccc

--- front/src/redux/slices/authSlice.js
+++ front/src/redux/slices/authSlice.js
@@ -1,27 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit"; // 1. slice 생성
-import Cookies from "js-cookie";
+import { createSlice } from '@reduxjs/toolkit'; // 1. slice 생성
+import Cookies from 'js-cookie';
 
 const initialState = {
-  authData: Cookies.get("authData")
-    ? JSON.parse(Cookies.get("authData")) //Cookies.get을 해서 authData가 있으면 바로 파싱
+  authData: Cookies.get('authData')
+    ? JSON.parse(Cookies.get('authData')) //Cookies.get을 해서 authData가 있으면 바로 파싱
     : null, //없으면 null
 };
 
 const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
     login: (state, action) => {
       //update 상태값 변경
       state.authData = action.payload.authData;
-      Cookies.set("authData", JSON.stringify(action.payload.authData), {
+      Cookies.set('authData', JSON.stringify(action.payload.authData), {
         expires: 1, //1일 동안 쿠키 저장 (쿠키의 유효기간)
       });
     },
     logout: (state) => {
       //상태값 비움
       state.authData = null;
-      Cookies.remove("authData");
+      Cookies.remove('authData');
     },
   },
 }); // slice 생성
