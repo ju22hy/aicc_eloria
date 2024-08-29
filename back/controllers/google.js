740854730bc794841e603b4a8d5b63e7e264ca5e
BY1502 <sks14502@gmail.com>
2024-08-29 14:16:54 +0900

    구글 연동

--- back/controllers/google.js
+++ back/controllers/google.js
@@ -13,35 +13,37 @@ passport.use(
       try {
         const { id, displayName, emails } = profile;
         const email = emails[0].value;
-        const password = '';
-        const contact = '';
 
         const results = await pool.query(
           'INSERT INTO aicc_5team (user_key, nickname, email, password, phone_number) VALUES ($1, $2, $3, $4, $5) ON CONFLICT (email) DO UPDATE SET nickname = $2 RETURNING *',
-          [id, displayName, email, password, contact]
+          [id, displayName, email, '', '']
         );
         const user = results.rows[0];
         return done(null, user);
       } catch (err) {
-        done(err, null);
+        return done(err, null);
       }
     }
   )
 );
 
 passport.serializeUser((user, done) => {
+  console.log('serial', user);
   done(null, user.email); // 세션에 저장할 데이터
 });
 
 passport.deserializeUser(async (email, done) => {
+  // console.log('Deserializing user with email:', email);
   try {
     const results = await pool.query(
       'SELECT * FROM aicc_5team WHERE email = $1',
       [email]
     );
     const user = results.rows[0];
+    // console.log('User found:', user);
     done(null, user);
   } catch (err) {
+    console.error('Error during deserialization:', err);
     done(err, null);
   }
 });
