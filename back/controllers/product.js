38ae6e70af4bafe99e90903f50b6b446f4d05ee1
BY1502 <sks14502@gmail.com>
2024-08-29 17:34:43 +0900

    메인페이지 데이터베이스 작업

--- back/controllers/product.js
+++ back/controllers/product.js
@@ -1,1 +1,19 @@
-const database = require('../database/database');
+const pool = require('../database/database');
+const express = require('express');
+const router = express.Router();
+
+router.get('/products', async (req, res) => {
+  try {
+    // 필요한 컬럼만 선택해서 데이터베이스 쿼리
+    const result = await pool.query(
+      'SELECT productname, productprice, productimage FROM product'
+    );
+    // 결과를 JSON 형태로 반환
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
--- back/index.js
+++ back/index.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 const authRoutes = require('./routes/auth');
 const PORT = 8080;
 const app = express();
+const productRoutes = require('./controllers/product');
+const path = require('path');
 
 app.use(
   session({
@@ -34,10 +36,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/auth', authRoutes); // 구글 라우트
+app.use('/api', productRoutes); // 상품 라우트
+app.use('/img/back/img', express.static(path.join(__dirname, '/img')));
 
 // 기존 라우트 설정
 app.use('/basket', require('./routes/basketroutes'));
 app.use('/login', require('./routes/loginroutes'));
 app.use('/signup', require('./routes/signUproutes'));
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`)); // 서버 실행 시 메시지
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+}); // 서버 실행 시 메시지
