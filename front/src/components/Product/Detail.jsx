7f04e62f01ce387d7f8baf3532ae9f52dc70de20
sora <ju22hy@gmail.com>
2024-08-30 11:57:19 +0900

    Detail 구조 수정, Parallax 적용, css 수정

--- front/src/components/Product/Detail.jsx
+++ front/src/components/Product/Detail.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./detail.css";
-import { LuPlus } from "react-icons/lu";
-import { LuMinus } from "react-icons/lu";
+import { LuPlus, LuMinus } from "react-icons/lu";
 import Detail1 from "../SubImage/product-ring1.jpg";
-// import Detail2 from "./img/detail-img2.png";
-// import Detail3 from "./img/detail-img3.png";
+import { useNavigate } from "react-router-dom";
 
 const Detail = () => {
-  const [quantity, setQuantity] = useState(0);
+  const stickyRef = useRef(null);
+  const staticRef = useRef(null);
+  const navigate = useNavigate();
+
+  const [quantity, setQuantity] = useState(1);
   const unitPrice = 12345; // 단가 설정
 
   // 총 가격 계산 함수
@@ -27,26 +29,60 @@ const Detail = () => {
     }
   };
 
-  // 버튼 클릭 핸들러
+  // BUY NOW 버튼 클릭 핸들러
   const handleBuyNow = () => {
     console.log("BUY NOW 버튼이 클릭되었습니다.");
+    navigate("/");
   };
 
+  // ADD TO CART 버튼 클릭 핸들러
   const handleAddToCart = () => {
-    console.log("ADD TO CART 버튼이 클릭되었습니다.");
+    const userConfirmed = window.confirm(
+      "장바구니에 상품이 담겼습니다. 장바구니 페이지로 이동하시겠습니까?"
+    );
+
+    if (userConfirmed) {
+      navigate("/cart");
+    }
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const anchor = staticRef.current;
+      const sticky = stickyRef.current;
+
+      if (anchor && sticky) {
+        const staticRect = anchor.getBoundingClientRect();
+        // check if the element is above the client's viewport
+        if (staticRect.top < 0) {
+          const ratio = (staticRect.top / staticRect.height) * -1;
+          const stickyRect = sticky.getBoundingClientRect();
+          if (window.innerHeight < stickyRect.height) {
+            const relativeHeight = stickyRect.height - window.innerHeight;
+            const offset = ratio * relativeHeight;
+            sticky.style.top = `-${offset}px`;
+          } else {
+            sticky.style.top = "0";
+          }
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className="detail-container">
-      <div className="image-container">
+      <div className="image-container" ref={stickyRef}>
         <div className="image-gallery">
-          <img src={Detail1} alt="제품 이미지" />
-          <img src={Detail1} alt="제품 추가 이미지 1" />
-          <img src={Detail1} alt="제품 추가 이미지 2" />
-          <img src={Detail1} alt="제품 추가 이미지 3" />
+          <img className="main-p-img" src={Detail1} alt="제품 이미지" />
+          <img className="concept-img" src={Detail1} alt="제품 추가 이미지 1" />
         </div>
       </div>
-      <div className="product-details">
+      <div className="product-details" ref={staticRef}>
         <div className="details-text-container">
           <h1 className="details-product-title">상품명상품명상품명</h1>
           <p className="details-product-price">
@@ -62,17 +98,11 @@ const Detail = () => {
             중앙에 자리한 화려한 큐빅 지르코니아 스톤이 반짝이는 빛을 발산하여
             손끝에서부터 우아함을 더해줍니다. <br />
             미니멀한 디자인으로 어떤 스타일에도 자연스럽게 어울리며, 단독으로
-            착용해도 충분히 돋보입니다.
-            <br />
-            심플한 라인과 정교한 세공은 트렌드를 타지 않는 클래식한 아름다움을
-            선사합니다.
+            착용해도 충분히 돋보입니다. 심플한 라인과 정교한 세공은 트렌드를
+            타지 않는 클래식한 아름다움을 선사합니다.
             <br />
             <br />
-            소중한 사람에게 선물로도 완벽하며, 일상 속에서 특별한 순간을 더욱
-            빛나게 만들어 줄 것입니다. <br />
-            이 반지는 여성스러움을 강조하면서도 모던한 감각을 잃지 않는
-            디자인으로, <br />
-            특별한 날뿐만 아니라 일상에서도 착용 가능한 최고의 선택입니다.
+            일상에서도 착용 가능한 최고의 선택입니다.
           </p>
           <div className="quantity-selector">
             <div className="quantity-input">
@@ -82,6 +112,7 @@ const Detail = () => {
                 name="quantity"
                 value={quantity}
                 min="1"
+                readOnly
               />
               <LuPlus className="plus-icon" onClick={handleIncreaseQuantity} />
               <LuMinus
@@ -94,12 +125,12 @@ const Detail = () => {
             TOTAL: KRW {calculateTotalPrice().toLocaleString()}
           </p>
           <div className="button-container">
-            <a href="#" className="buy-now" onClick={handleBuyNow}>
+            <button className="buy-now" onClick={handleBuyNow}>
               BUY NOW
-            </a>
-            <a href="#" className="add-to-cart" onClick={handleAddToCart}>
+            </button>
+            <button className="add-to-cart" onClick={handleAddToCart}>
               ADD TO CART
-            </a>
+            </button>
           </div>
         </div>
       </div>
